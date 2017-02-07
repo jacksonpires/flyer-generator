67936689e85388df6e7362d254ef2ab3410b0da5
Jackson Pires <jackson.pires@gmail.com>
2017-02-07 11:14:33 +0000

    Adicionado rota para apagar convites em branco

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,2 +1,6 @@
 class AboutController < ApplicationController
+  def clear
+    Flyer.where(address1: '').delete_all
+    render text: "OK!"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :flyers
   resources :about, only: [:index]
   
+  get 'about/clear'
+  
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
